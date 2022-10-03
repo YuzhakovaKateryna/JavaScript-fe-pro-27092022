c832445af997931828e430b379d7a613fa769a19
YuzhakovaKateryna <feduleeva.ekaterina1706@gmail.com>
2022-10-03 16:53:49 +0300

    second commit

---
+++ 01/main.js
@@ -0,0 +1,6 @@
+let firstVar =prompt ('firstVar','User name');
+console.log(firstVar);
+document.writeln(firstVar);
+let secondVar =prompt ('secondVar','User last name');
+console.log(secondVar);
+document.writeln(secondVar);
