ea19c71656b0de1f9f9611d59b7d067f2815476e
YuzhakovaKateryna <feduleeva.ekaterina1706@gmail.com>
2022-10-03 16:56:30 +0300

    second commit

--- compare.js
+++
@@ -1,5 +0,0 @@
-let Name= "true";
-let Имя="false";
-
-console.log("Uk",Name!=Имя);
-console.log("Ru",Name=Имя);
--- main.js
+++
@@ -1,6 +0,0 @@
-let firstVar =prompt ('firstVar','User name');
-console.log(firstVar);
-document.writeln(firstVar);
-let secondVar =prompt ('secondVar','User last name');
-console.log(secondVar);
-document.writeln(secondVar);
