540b50cdcc807c6769b8b732dffd52cee9b0d441
YuzhakovaKateryna <114920827+YuzhakovaKateryna@users.noreply.github.com>
2022-10-20 21:02:50 +0300

    delete file

--- 04/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let randomArrNumbers = [];
-function randomArray(randomArray){
-while (randomArray.length=10) {
-  Math.ceil(Math.random(randomArray)*100);
-}
-console.log(randomArray);
-}
-randomArray(randomArrNumbers);
